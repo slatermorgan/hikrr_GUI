96a4adee9783cadea355f0098451a4361ea4feac
slatermorgan <slatermorgan@hotmail.com>
2020-04-20 20:54:48 +0100

    Make rows components as they should be

--- src/components/common/ErrorRow.js
+++ src/components/common/ErrorRow.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import TableCell from '@material-ui/core/TableCell';
 import TableRow from '@material-ui/core/TableRow';
 
-const ErrorRow = (options) => {
-    const error = options.error;
-
+class ErrorRow extends Component {
+    render() {
         return (
-        <TableRow key="error-row">
-            <TableCell>{error && 'Error'}</TableCell>
-        </TableRow>
-    );
+            <TableRow key="error-row">
+                <TableCell>{this.props.error && 'Error'}</TableCell>
+            </TableRow>
+        );
+    }
 }
 
 export default ErrorRow

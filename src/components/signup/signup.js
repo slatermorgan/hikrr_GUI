4b1f37a283a9db0e09aa0a49567e2d669ff6b00b
slatermorgan <slatermorgan@hotmail.com>
2020-04-28 22:01:15 +0100

    Create basic adaptive sign up view

--- src/components/signup/signup.js
+++ src/components/signup/signup.js
@@ -1,32 +1,60 @@
 import React, {Component } from 'react';
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles, StylesProvider } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles({
-  inputSearch: {
-    color: 'red',
-  },
-  inputSearch_Div : {
-    width: '100vw !important',
-    margin: 20,
-  }
-});
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+
+const useStyles = {
+    inputSearch: {
+        color: 'red',
+    },
+    inputSearch_Div : {
+        width: '100vw !important',
+        margin: 20,
+    },
+    login_Grid : {
+        paddingTop: '100px',
+    },
+    login_Div : {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
+};
 
 class SignUp extends Component {
 
     render()  {
         return (
-            <Grid container spacing={5}>
-                <Grid item xs={3}>
+            <Grid container spacing={5} style={useStyles.login_Grid}>
+                <Grid item xs={2} md={3}>
                     <Paper />
                 </Grid>
-                <Grid item xs={6}>
+                <Grid item xs={8} md={6}>
                     <Paper>
-                        login here
+                        <div style={useStyles.login_Div}>
+                            <form noValidate>
+                                <div>
+                                    <FormControl>
+                                        <InputLabel htmlFor="component-username">Username</InputLabel>
+                                        <Input type="text" id="component-username"/>
+                                    </FormControl>
+                                </div>
+                                <div>
+                                    <FormControl>
+                                        <InputLabel htmlFor="component-password">Password</InputLabel>
+                                        <Input type="password" id="component-password"/>
+                                    </FormControl>
+                                </div>
+                            </form>
+
+                        </div>
                     </Paper>
                 </Grid>
-                <Grid item xs={3}>
+                <Grid item xs={2} md={3}>
                     <Paper />
                 </Grid>
             </Grid>

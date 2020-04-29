f586d7ca1500940122ab584c99a5bacb70cbba2b
slatermorgan <slatermorgan@hotmail.com>
2020-04-29 20:37:06 +0100

    Add basic Jumbotron to index view

--- src/components/index/index.js
+++ src/components/index/index.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Jumbotron from 'react-bootstrap/Jumbotron';
 
 const Index = () => {
     return (
-        <h1>Welcome to Hikrr</h1>
+        <>
+            <style type="text/css">
+                {`
+                .btn-signup {
+                    background-color: #94420c;
+                }
+
+                .btn-browse {
+                    background-color: #27940c;
+                }
+                `}
+            </style>
+            <Jumbotron>
+                <h1>Welcome to Hikrr</h1>
+                <p>
+                    The beauty of nature doesn’t need to be hard to find. Our goal is simple - build the largest collection mountain trails, so you can explore the outdoors with confidence. Anytime. Anywhere.
+                </p>
+                <p>
+                    <Button className="m-2" variant="signup">Sign Up</Button>
+                    <Button className="m-2" variant="browse">Browse Our Peaks</Button>
+                </p>
+            </Jumbotron>
+        </>
+
     );
 };
 

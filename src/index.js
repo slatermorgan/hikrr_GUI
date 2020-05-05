4057dc3a840fe049eb6b3ec85270ba0eb941ded8
Tom-Corkhill <63905667+Tom-Corkhill@users.noreply.github.com>
2020-05-05 17:20:18 +0100

    Add test PR

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// Test PR
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA

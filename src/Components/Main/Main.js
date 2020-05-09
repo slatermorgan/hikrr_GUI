66a9b6c8ada214fad2e54076d142289c4cb1862b
slatermorgan <slatermorgan@hotmail.com>
2020-05-09 12:26:50 +0100

    Add create an account axios call

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Switch, Route, withRouter } from 'react-router-dom';
 
 import Index from '../Index';
@@ -7,16 +7,27 @@ import Peak from '../Peak';
 import SignUp from '../Signup/Signup';
 import Signin from '../Signin';
 
-const Main = () => (
-    <Switch>
-        <Route exact path="/" component={ withRouter(Index) } />
-        <Route path="/signup" component={ withRouter(SignUp) } />
-        <Route exact path="/peaks" component={ withRouter(Peaks) } />
-        <Route path="/peaks/:id" children={<Peak/>} />
-        <Route path="/login" component={ withRouter(Signin) } />
-        <Route path="/about"/>
-        <Route path="/contact"/>
-    </Switch>
-);
+const Main = () => {
+    const [title, updateTitle] = useState(null);
+    const [errorMessage, updateErrorMessage] = useState(null);
+
+    return (
+        <Switch>
+            <Route exact path="/" component={ withRouter(Index) } />
+            {/* <Route path="/signup" component={ withRouter(SignUp) } /> */}
+            <Route exact path="/peaks" component={ withRouter(Peaks) } />
+            <Route path="/peaks/:id" children={<Peak/>} />
+            <Route path="/login" component={ withRouter(Signin) } />
+            <Route path="/about"/>
+            <Route path="/contact"/>
+            <Route path="/signup">
+                <SignUp
+                    showError={updateErrorMessage}
+                    updateTitle={updateTitle}
+                />
+            </Route>
+        </Switch>
+    );
+};
 
 export default Main;
